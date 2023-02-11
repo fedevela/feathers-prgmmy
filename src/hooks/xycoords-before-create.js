e39fc9e6b7dcd8631a4c35e454fb25866fe7b6b8
Federico Vela <fedevela@gmail.com>
2023-02-11 02:55:57 -0500

    with drag

--- src/hooks/xycoords-before-create.js
+++ src/hooks/xycoords-before-create.js
@@ -3,7 +3,18 @@
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-  return async context => {
+  return async (context) => {
+    const { data } = context;
+
+    // Throw an error if we didn't get a text
+    if (!data.x) {
+      throw new Error("An X coordinate must be set.");
+    }
+    if (!data.x) {
+      throw new Error("A Y coordinate must be set.");
+    }
+    context.data.createdAt = new Date().getTime();
+
     return context;
   };
 };
